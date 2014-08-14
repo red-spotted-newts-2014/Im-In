9597dec89cac97d3b4d19fec802fb1f9c87872d1
Fahia Mohamed  & Stella Kim <pair+fahiamoh.stellajkimm@devbootcamp.com>
2014-08-14 15:44:14 -0400

    removed devise, added bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable
+  has_secure_password
+  acts_as_follower
+  acts_as_followable
+
   has_many :invitations
   has_many :events, through: :invitations
   has_many :created_events, class_name: :Event, foreign_key: :user_id
