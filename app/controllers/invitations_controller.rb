b35bb5991e96a55c90a44ab2b1d6859205dbc401
Christian Moon & Fahia Mohamed <pair+cmoon820.fahiamoh@devbootcamp.com>
2014-08-14 20:50:39 -0400

    add invitation#create, updated routes to add invitations

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,2 +1,12 @@
 class InvitationsController < ApplicationController
+  def create
+    @event = Event.find(params[:id])
+    @invitation = @event.invitations.create(invitation_params)
+  end
+
+  private
+
+  def invitation_params
+    params.require(:invitation).permit(:user_id, :status)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,18 @@ Rails.application.routes.draw do
 
   root 'users#index'
 
-  resources :events
+  resources :events do
+    resources :invitations, only: [:create]
+  end
+  resources :users
 
   post 'users/login' => 'users#login', as: 'login'
-
   get 'users/logout' => 'users#logout', as: 'logout'
-
   post 'users/follow' => 'users#follow', as: 'follow'
-
   post 'users/unfollow' => 'users#unfollow', as: 'unfollow'
-
   get 'users/:id/attending' => 'users#attending', as: "user_attending"
-
   get 'users/:id/created' => 'users#created', as: "user_created"
 
-  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
