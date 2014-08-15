ec8afa3846cf2722b5cdb9e5bd9d2cec7820ec19
Stella Kim <stellajkimm@gmail.com>
2014-08-15 17:29:28 -0400

    hardcoded current_user to user1 to test swift

--- app/helpers/sessions.rb
+++ app/helpers/sessions.rb
@@ -1,5 +1,6 @@
 module SessionsHelper
   def current_user
-    @user ||= User.find(session[:id]) if session[:id]
+    # @user ||= User.find(session[:id]) if session[:id]
+    @user ||= User.find(1)
   end
 end
