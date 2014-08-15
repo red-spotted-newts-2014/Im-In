3c6413e5aa4dc1334b58abe0fb55ba00f25680e9
stellajkimm <stellajkimm@gmail.com>
2014-08-15 13:36:02 -0400

    nothing works.

--- im-in-ios/im-in-ios/EventViewController.swift
+++ im-in-ios/im-in-ios/EventViewController.swift
@@ -14,25 +14,40 @@ class EventViewController: UIViewController, APIControllerProtocol, UITableViewD
     
     @IBOutlet weak var button: UIButton!
     var apiCtrl = APIController()
+    var invitedEvents: AnyObject!
     
     func didReceiveAPIResults(results: NSDictionary) {
         println(results)
+        invitedEvents = results.objectForKey("invited_events")
+        println("****")
+        println(invitedEvents)
     }
-    
+//    
+//    var results: AnyObject! = ["user" : [String : AnyObject]()]
+//    let responseDic = response as [String : AnyObject]
+//    let userDic: [String : AnyObject] = responseDic["user"]! as [String : AnyObject]
+//    
     @IBAction func buttonPressed(sender: AnyObject) {
         apiCtrl.loadAllEvents()
         
     }
     
     func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
-        return 0;
+        return 3;
     }
     
     // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
     // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
     
     func tableView(tableView:UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
-        return nil;
+        
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+//        println("***")
+        println(invitedEvents)
+        cell.textLabel.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel.text = "Subtitle #\(indexPath.row)"
+        
+        return cell
     }
     
     
