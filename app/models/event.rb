678cffa916fa8156005cc34d03a7a6e0029f7625
Stella Kim <stellajkimm@gmail.com>
2014-08-15 16:33:21 -0400

    add username in events

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,10 @@ class Event < ActiveRecord::Base
   has_many :invitations
   has_many :invitees, through: :invitations, source: :user
 
+  before_save :add_username
+
+  def add_username
+    username = User.find(user_id).username
+  end
+
 end
