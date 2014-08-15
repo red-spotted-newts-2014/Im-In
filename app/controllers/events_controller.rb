6b416ca789118869d9a37a19d772a90a16989e2d
Stella Kim <stellajkimm@gmail.com>
2014-08-15 10:45:36 -0400

    edited events controller to have actions that only return separate json objects for created,invited, and attending

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,17 +1,7 @@
 class EventsController < ApplicationController
 
   def index
-    # all events in feed
     @invited_events = current_user.invited_events
-    # all events user is "in"
-    @attending_invitations = current_user.invitations.where(status: "in")
-    @attending_events = []
-    @attending_invitations.each do |invite|
-      @attending_events << Event.find(invite.event_id)
-    end
-
-    # all events user created
-    @created_events = current_user.created_events
 
     respond_to do |format|
       format.html
@@ -51,6 +41,26 @@ class EventsController < ApplicationController
     redirect_to events_path
   end
 
+  def attending
+    @attending_invitations = current_user.invitations.where(status: "in")
+    @attending_events = []
+    @attending_invitations.each do |invite|
+      @attending_events << Event.find(invite.event_id)
+
+     respond_to do |format|
+       format.html
+       format.json { render :json => { :events => @events }}
+     end
+   end
+
+   def created
+     @events = current_user.created_events
+
+     respond_to do |format|
+       format.html
+       format.json { render :json => { :events => @events }}
+     end
+   end
   private
 
   def event_params
