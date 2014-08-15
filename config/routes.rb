3b87ca7bf98d374a8dcb32e15ea3cb1f766e4c50
Stella Kim <stellajkimm@gmail.com>
2014-08-15 17:51:18 -0400

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
   resources :events do
     resources :invitations, only: [:create, :new]
   end
-  resources :users
 
   post 'users/login' => 'users#login', as: 'login'
   get 'users/logout' => 'users#logout', as: 'logout'
   post 'users/follow' => 'users#follow', as: 'follow'
   post 'users/unfollow' => 'users#unfollow', as: 'unfollow'
+
   get 'users/:id/events_attending' => 'users#events_attending', as: "user_events_attending"
   get 'users/:id/created' => 'users#created', as: "user_events_created"
   get 'events/:id/friends_attending' => 'events#friends_attending', as: "events_friends_attending"
+
+  resources :users
 end
