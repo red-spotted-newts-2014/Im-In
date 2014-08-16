4769329bec7712688ce224c458fd55cac65d663b
elz <joelyawili@hotmail.com>
2014-08-15 22:51:25 -0400

    create view of friends we follow

--- im-in-ios/im-in-ios/FollowingViewController.swift
+++ im-in-ios/im-in-ios/FollowingViewController.swift
@@ -1,9 +1,76 @@
 //
-//  FriendsViewController.swift
-//  im-in-ios
+//  ViewController.swift
+//  Im_in
 //
-//  Created by joel yawili on 8/15/14.
-//  Copyright (c) 2014 joel yawili. All rights reserved.
+//  Created by fahia mohamed on 2014-08-14.
+//  Copyright (c) 2014 fahia mohamed. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+//
+//, APIControllerProtocol
+class FollowingViewController: UIViewController, APIFollowingControllerProtocol, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var button: UIButton!
+    var apiCtrl = APIFollowingController()
+    var invitedEvents: NSArray!
+    
+    func didReceiveAPIResults(results: NSDictionary) {
+        println("FollowingViewController#didReceiveAPIResults")
+        //        println(results)
+        invitedEvents = results.objectForKey("following") as? NSArray
+        //        println("****")
+        println(invitedEvents)
+        self.tableView.reloadData()
+    }
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        //apiCtrl.loadAllEvents()
+        
+    }
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        println("FollowingViewController#tableView (count)")
+        if invitedEvents == nil {
+            return 0
+        }
+        return invitedEvents.count;
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(tableView:UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        
+        println("FollowingViewController#tableView")
+        
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "MyTestCell")
+        //        println("***")
+        
+        //        cell.textLabel.text = "Row #\(indexPath.row)"
+        cell.textLabel.text = invitedEvents[indexPath.row].objectForKey("username") as? String
+        
+        
+        return cell
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("FollowingViewController#viewDidLoad")
+        apiCtrl.delegate = self
+        apiCtrl.loadAllEvents()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
