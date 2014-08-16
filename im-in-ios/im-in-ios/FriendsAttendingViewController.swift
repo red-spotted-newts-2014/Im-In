ec6f923e3aa59581b5a17df5bae3f27008ce7e7e
elz <joelyawili@hotmail.com>
2014-08-16 01:30:17 -0400

    add friends attending view

--- im-in-ios/im-in-ios/FriendsAttendingViewController.swift
+++ im-in-ios/im-in-ios/FriendsAttendingViewController.swift
@@ -1,9 +1,77 @@
 //
-//  FriendsAttendingViewController.swift
-//  im-in-ios
+//  ViewController.swift
+//  Im_in
 //
-//  Created by joel yawili on 8/16/14.
-//  Copyright (c) 2014 joel yawili. All rights reserved.
+//  Created by fahia mohamed on 2014-08-14.
+//  Copyright (c) 2014 fahia mohamed. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+//
+//, APIControllerProtocol
+class FriendsAttendingViewController: UIViewController, APIFriendsAttendingControllerProtocol, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var button: UIButton!
+    var apiCtrl = APIFriendsAttendingController()
+    var attendingFriends: NSArray!
+    
+    func didReceiveAPIResults(results: NSDictionary) {
+        println("FriendsAttendingViewController#didReceiveAPIResults")
+        //        println(results)
+        attendingFriends = results.objectForKey("friends_attending") as? NSArray
+        //        println("****")
+        println(attendingFriends)
+        self.tableView.reloadData()
+    }
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        //apiCtrl.loadAllEvents()
+        
+    }
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        println("FriendsAttendingViewController#tableView (count)")
+        if attendingFriends == nil {
+            return 0
+        }
+        return attendingFriends.count;
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(tableView:UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        
+        println("FriendsAttendingViewController#tableView")
+        
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "MyTestCell")
+        //        println("***")
+        
+        //        cell.textLabel.text = "Row #\(indexPath.row)"
+        cell.textLabel.text = attendingFriends[indexPath.row].objectForKey("user_id") as? String
+        cell.detailTextLabel.text = attendingFriends[indexPath.row].objectForKey("status") as? String
+        
+        
+        return cell
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("FriendsAttendingViewController#viewDidLoad")
+        apiCtrl.delegate = self
+        apiCtrl.loadAllEvents()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
