c3f3ffd78f1faba96a4f9b1b674348c98e9c9d09
elz <joelyawili@hotmail.com>
2014-08-16 20:49:42 -0400

    add API for user profile view

--- im-in-ios/im-in-ios/APIUserProfileViewController.swift
+++ im-in-ios/im-in-ios/APIUserProfileViewController.swift
@@ -1,9 +1,45 @@
 //
-//  APIUserProfile.swift
-//  im-in-ios
+//  APIController.swift
+//  helloworld
 //
-//  Created by joel yawili on 8/16/14.
-//  Copyright (c) 2014 joel yawili. All rights reserved.
+//  Created by fahia mohamed on 2014-08-14.
+//  Copyright (c) 2014 fahia mohamed. All rights reserved.
 //
 
 import Foundation
+
+protocol APIUserProfileViewControllerProtocol {
+    func didReceiveAPIResults(results: NSDictionary)
+}
+
+class APIUserProfileViewController {
+    
+    var delegate: APIUserProfileViewControllerProtocol?
+    
+    init() {
+        
+    }
+    
+    func loadAllEvents() {
+        println("APIController#loadAllEvents")
+        let urlPath = "http://localhost:3000/users/1.json"
+        let url: NSURL = NSURL(string: urlPath)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
+            println("Task completed")
+            if(error) {
+                // If there is an error in the web request, print it to the console
+                println(error.localizedDescription)
+            }
+            var err: NSError?
+            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+            if(err != nil) {
+                // If there is an error parsing JSON, print it to the console
+                println("JSON Error \(err!.localizedDescription)")
+            }
+            self.delegate?.didReceiveAPIResults(jsonResult) // THIS IS THE NEW LINE!!
+        })
+        task.resume()
+    }
+    
+}
