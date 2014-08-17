d5bc8f54e9eb55df9d85c2692e589e5b014f1c84
elz <joelyawili@hotmail.com>
2014-08-17 18:31:29 -0400

    Add API to Add Friends

---
+++ im-in-ios/im-in-ios/AddFriendsViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Im_in
+//
+//  Created by fahia mohamed on 2014-08-14.
+//  Copyright (c) 2014 fahia mohamed. All rights reserved.
+//
+
+import UIKit
+
+//
+//, APIControllerProtocol
+class AddFriendsViewController: UIViewController, APIAddFriendsControllerProtocol, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var button: UIButton!
+    var apiCtrl = APIAddFriendsController()
+    var attending: NSArray!
+    
+    func didReceiveAPIResults(results: NSDictionary) {
+        println("AddFriendsViewController#didReceiveAPIResults")
+        println(results)
+        attending = results.objectForKey("following") as? NSArray
+        //        println("****")
+        println(attending)
+        self.tableView.reloadData()
+    }
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        //apiCtrl.loadAllEvents()
+        
+    }
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        println("AddFriendsViewController#tableView (count)")
+        if attending == nil {
+            return 0
+        }
+        return attending.count;
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(tableView:UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        
+        println("AddFriendsViewController#tableView")
+        
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "MyTestCell")
+        //        println("***")
+        
+        //        cell.textLabel.text = "Row #\(indexPath.row)"
+        cell.textLabel.text = attending[indexPath.row].objectForKey("username") as? String
+        
+        
+        return cell
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("AddFriendsViewController#viewDidLoad")
+        apiCtrl.delegate = self
+        apiCtrl.loadAllEvents()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
