e5c23dd2ec79a0dc4a69cf1933636f23422787dc
elz <joelyawili@hotmail.com>
2014-08-18 04:44:34 -0400

    fixed segue to event detail page !!!YES!!!

--- im-in-ios/im-in-ios/EventDetailViewController.swift
+++ im-in-ios/im-in-ios/EventDetailViewController.swift
@@ -10,21 +10,39 @@ import UIKit
 
 class EventDetailViewController: UIViewController {
 
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var descTextField: UITextView!
+    @IBOutlet var venueTextField: UITextField!
+    @IBOutlet var locationTextField: UITextField!
+    @IBOutlet var startTextField: UITextField!
+    @IBOutlet var endTextField: UITextField!
+    
+    var eventData:NSDictionary = NSDictionary()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-      
-        // Do any additional setup after loading the view.
+        nameTextField.userInteractionEnabled = false
+        descTextField.userInteractionEnabled = false
+        venueTextField.userInteractionEnabled = false
+        locationTextField.userInteractionEnabled = false
+        startTextField.userInteractionEnabled = false
+        endTextField.userInteractionEnabled = false
+        
+        nameTextField.text = eventData.objectForKey("name") as String
+        descTextField.text = eventData.objectForKey("description") as String
+        venueTextField.text = eventData.objectForKey("venue") as String
+        locationTextField.text = eventData.objectForKey("location") as String
+        startTextField.text = eventData.objectForKey("start_time") as String
+        endTextField.text = eventData.objectForKey("end_time") as String
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
-    }
+}
 
 
 
