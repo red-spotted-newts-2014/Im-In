4ad307808b6b7a286c73ec010fe59b33b55f1ae0
fahia mohamed <fahia_mohamed@hotmail.com>
2014-08-18 13:39:59 -0400

    Add user check to match input to logged data.  Working towards checking user input to backend database.

--- im-in-ios/im-in-ios/untitled folder/LogInViewController.swift
+++ im-in-ios/im-in-ios/untitled folder/LogInViewController.swift
@@ -14,17 +14,36 @@ class LogInViewController: UIViewController, APILogInControllerProtocol, NSURLCo
     
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var logInButton: UIButton!
     
     func didReceiveAPIResults(results: NSDictionary) {
         println("LogInViewController#didReceiveAPIResults")
     }
+    @IBOutlet weak var incorrectEntry: UITextField!
     
-    @IBAction func buttonPressed(sender: AnyObject) {
-        println("pressed")
-        var params = ["username": usernameField.text, "password": passwordField.text] as Dictionary
-        apiCtrl.sendLogInInfo(params)
+    @IBAction func loginButton(sender: AnyObject) {
+        var usr = "usr"
+        var pass = "pass"
+        
+        if usernameField.text == usr &&
+            passwordField.text == pass
+        {
+            
+            usernameField.resignFirstResponder()
+            passwordField.resignFirstResponder()
+            
+        }
+        else
+        {
+            incorrectEntry.text = "Incorrect username/password"
+            usernameField.resignFirstResponder()
+            passwordField.resignFirstResponder()
     }
+//    @IBAction func buttonPressed(sender: AnyObject) {
+//        println("pressed")
+//        var params = ["username": usernameField.text, "password": passwordField.text] as Dictionary
+//        apiCtrl.sendLogInInfo(params)
+    }
+    
     
 }
 
