f3f1a122a8dd5cbe08c7679d26b4af198ed76ad7
elz <joelyawili@hotmail.com>
2014-08-18 04:12:17 -0400

    add event details view

--- im-in-ios/im-in-ios/EventDetailViewController.swift
+++ im-in-ios/im-in-ios/EventDetailViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class EventShowViewController2: UIViewController {
+class EventDetailViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+      
         // Do any additional setup after loading the view.
     }
 
@@ -22,14 +24,7 @@ class EventShowViewController2: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
 
-}
+
+
