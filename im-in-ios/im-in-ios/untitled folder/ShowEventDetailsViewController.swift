e589687971e64e5b6dbd5d2a894b37920dacb5ff
Christian Moon <cmoon.nyc@gmail.com>
2014-08-18 21:48:22 -0400

    commented out showeventdetailsviewcontroller because it is unused

--- im-in-ios/im-in-ios/untitled folder/ShowEventDetailsViewController.swift
+++ im-in-ios/im-in-ios/untitled folder/ShowEventDetailsViewController.swift
@@ -1,85 +1,85 @@
+////
+////  ViewController.swift
+////  Im_in
+////
+////  Created by fahia mohamed on 2014-08-14.
+////  Copyright (c) 2014 fahia mohamed. All rights reserved.
+////
 //
-//  ViewController.swift
-//  Im_in
+//import UIKit
 //
-//  Created by fahia mohamed on 2014-08-14.
-//  Copyright (c) 2014 fahia mohamed. All rights reserved.
+////
+////, APIControllerProtocol
+//class ShowEventDetailsViewController: UIViewController, APIShowEventDetailsViewControllerProtocol, UITableViewDataSource, UITableViewDelegate {
+//    
+//    @IBOutlet weak var tableView: UITableView!
+//    @IBOutlet weak var button: UIButton!
+//    var apiCtrl = APIShowEventDetailsViewController()
+//    var details: NSArray!
+//    var users: NSArray! 
+//    
+//    func didReceiveAPIResults(results: NSDictionary) {
+//        println("ShowEventDetailsViewController#didReceiveAPIResults")
+//                println(results)
+//        details = results.objectForKey("event") as? NSArray
+//        users = results.objectForKey("users") as? NSArray
+//        //        println("****")
+//        println(details)
+//        dispatch_async(dispatch_get_main_queue(),{
+//            self.tableView.reloadData()
+//        })
+//    }
+//    
+//    
+//    @IBAction func buttonPressed(sender: AnyObject) {
+//        //apiCtrl.loadAllEvents()
+//        
+//    }
+//    
+//    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+//        println("ShowEventDetailsViewController#tableView (count)")
+//        if details == nil {
+//            return 0
+//        }
+//        return details.count;
+//    }
+//    
+//    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+//    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+//    
+//    func tableView(tableView:UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+//        
+//        println("ShowEventDetailsViewController#tableView")
+//        
+//        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Value2, reuseIdentifier: "MyTestCell")
+//        //        println("***")
+//        
+//        //        cell.textLabel.text = "Row #\(indexPath.row)"
+//        
+//        cell.textLabel.text = details[indexPath.row].objectForKey("description") as? String
+//        cell.detailTextLabel.text = details[indexPath.row].objectForKey("start_time") as? String
+//        //        cell.textLabel.text = users[indexPath.row].objectForKey("username") as? String USE FOR USERS DETAILS
 //
-
-import UIKit
-
+//        
+//        return cell
+//        
+//    }
+//    
+//    
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        println("ShowEventDetailsViewController#viewDidLoad")
+//        apiCtrl.delegate = self
+//        apiCtrl.loadAllEvents()
+//    }
+//    
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//    
+//    
+//}
 //
-//, APIControllerProtocol
-class ShowEventDetailsViewController: UIViewController, APIShowEventDetailsViewControllerProtocol, UITableViewDataSource, UITableViewDelegate {
-    
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var button: UIButton!
-    var apiCtrl = APIShowEventDetailsViewController()
-    var details: NSArray!
-    var users: NSArray! 
-    
-    func didReceiveAPIResults(results: NSDictionary) {
-        println("ShowEventDetailsViewController#didReceiveAPIResults")
-                println(results)
-        details = results.objectForKey("event") as? NSArray
-        users = results.objectForKey("users") as? NSArray
-        //        println("****")
-        println(details)
-        dispatch_async(dispatch_get_main_queue(),{
-            self.tableView.reloadData()
-        })
-    }
-    
-    
-    @IBAction func buttonPressed(sender: AnyObject) {
-        //apiCtrl.loadAllEvents()
-        
-    }
-    
-    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
-        println("ShowEventDetailsViewController#tableView (count)")
-        if details == nil {
-            return 0
-        }
-        return details.count;
-    }
-    
-    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
-    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
-    
-    func tableView(tableView:UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
-        
-        println("ShowEventDetailsViewController#tableView")
-        
-        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Value2, reuseIdentifier: "MyTestCell")
-        //        println("***")
-        
-        //        cell.textLabel.text = "Row #\(indexPath.row)"
-        
-        cell.textLabel.text = details[indexPath.row].objectForKey("description") as? String
-        cell.detailTextLabel.text = details[indexPath.row].objectForKey("start_time") as? String
-        //        cell.textLabel.text = users[indexPath.row].objectForKey("username") as? String USE FOR USERS DETAILS
-
-        
-        return cell
-        
-    }
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        println("ShowEventDetailsViewController#viewDidLoad")
-        apiCtrl.delegate = self
-        apiCtrl.loadAllEvents()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-}
-
-
+//
