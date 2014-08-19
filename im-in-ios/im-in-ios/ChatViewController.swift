be821f83b5abf57e21e00705785a22b183146a9e
Christian Moon <cmoon.nyc@gmail.com>
2014-08-19 18:41:32 -0400

    chat works

--- im-in-ios/im-in-ios/ChatViewController.swift
+++ im-in-ios/im-in-ios/ChatViewController.swift
@@ -9,7 +9,7 @@
 
 import UIKit
 
-class ChatViewController: UIViewController {
+class ChatViewController: GlobalController {
     
     var chat: NSMutableArray = NSMutableArray()
     var firebase: Firebase?
@@ -67,43 +67,5 @@ class ChatViewController: UIViewController {
         
         return cell
     }
-    override func viewWillAppear(animated: Bool) {
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
-    }
-    
-    override func viewWillDisappear(animated: Bool) {
-        NSNotificationCenter.defaultCenter().removeObserver(UIKeyboardWillShowNotification)
-        NSNotificationCenter.defaultCenter().removeObserver(UIKeyboardWillHideNotification)
-    }
-    
-    func keyboardWillShow(notification: NSNotification) {
-        self.moveView(notification.userInfo, up: true)
-    }
-    
-    func keyboardWillHide(notification: NSNotification) {
-        self.moveView(notification.userInfo, up: false)
-    }
-    
-    func moveView(userInfo: NSDictionary, up: Bool) {
-        var keyboardEndFrame: CGRect = CGRect()
-        userInfo[UIKeyboardFrameEndUserInfoKey].getValue(&keyboardEndFrame)
-        
-        var animationCurve: UIViewAnimationCurve = UIViewAnimationCurve.EaseOut
-        userInfo[UIKeyboardAnimationCurveUserInfoKey].getValue(&animationCurve)
-        
-        var animationDuration: NSTimeInterval = NSTimeInterval()
-        userInfo[UIKeyboardAnimationDurationUserInfoKey].getValue(&animationDuration)
-        
-        UIView.beginAnimations(nil, context: nil)
-        UIView.setAnimationBeginsFromCurrentState(true)
-        UIView.setAnimationDuration(animationDuration)
-        UIView.setAnimationCurve(animationCurve)
-        
-        var keyboardFrame: CGRect = self.view.convertRect(keyboardEndFrame, toView: nil)
-        var y = keyboardFrame.size.height * (up ? -1 : 1)
-        self.view.frame = CGRectOffset(self.view.frame, 0, y)
-        
-        UIView.commitAnimations()
-    }
+
 }
