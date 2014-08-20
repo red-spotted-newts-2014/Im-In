fa143b7a20801c64ccfc473e427ab898ec742c7f
elz <joelyawili@hotmail.com>
2014-08-20 16:45:54 -0400

    working login + fixing datePickers

--- im-in-ios/im-in-ios/CreateEventViewController.swift
+++ im-in-ios/im-in-ios/CreateEventViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class CreateEventViewController: UIViewController, UITextFieldDelegate
-//, APIAddEventControllerProtocol, NSURLConnectionDataDelegate, NSURLConnectionDelegate
-
-
+    //, APIAddEventControllerProtocol, NSURLConnectionDataDelegate, NSURLConnectionDelegate
+    
+    
 {
     
     override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
@@ -31,12 +31,98 @@ class CreateEventViewController: UIViewController, UITextFieldDelegate
         super.touchesBegan(touches, withEvent: event)
     }
     
+    
+    
+    @IBOutlet var nameTextField: UITextField!
+    
+    @IBOutlet var descTextField: UITextView!
+    @IBOutlet var venueTextField: UITextField!
+    
+    @IBOutlet var locationTextField: UITextField!
+    
+    @IBOutlet var startTexTField: UITextField!
+    
+    @IBOutlet var endTextField: UITextField!
+    
+    @IBOutlet var datePicker: UIDatePicker!
+    
+    @IBOutlet var endDatePicker: UIDatePicker!
+    
+    
+    @IBAction func startDateButton(sender: AnyObject) {
+        
+                hideEndDatePicker()
+                showDatePicker()
+        datePicker.hidden = false
+    }
+
+    @IBAction func endDateButton(sender: AnyObject) {
+        
+                hideStartDatePicker()
+                showEndDatePicker()
+        
+        endDatePicker.hidden = false
+    }
+    
+    
+    @IBAction func confirmDatesButton(sender: AnyObject) {
+        
+                startTexTField.hidden = false
+        
+                endTextField.hidden = false
+        
+                startTexTField.text = "\(datePicker.date)"
+                endTextField.text = "\(endDatePicker.date)"
+        
+                datePicker.hidden = true
+                endDatePicker.hidden = true
+    }
+    
+    
+    
+    func showDatePicker() {
+        println("showing Start Date Picker")
+        self.view.addSubview(datePicker)
+        datePicker.frame = CGRectMake(0,100,320, 50);
+        UIView.animateWithDuration(1.0, animations: animStartDatePicker)
+        datePicker.backgroundColor = UIColor( white:1, alpha:1)
+        self.view.backgroundColor = UIColor(white:1, alpha: 1)
+        endDatePicker.hidden = true
+    }
+    
+    func showEndDatePicker() {
+        println("showing End Date Picker")
+        self.view.addSubview(endDatePicker)
+        endDatePicker.frame = CGRectMake(0,100,320, 50);
+        UIView.animateWithDuration(1.0, animations: animEndDatePicker)
+        endDatePicker.backgroundColor = UIColor( white:1, alpha:1)
+        self.view.backgroundColor = UIColor(white:1, alpha: 1)
+        datePicker.hidden = false
+    }
+    
+    
+    
+    
+    var params: NSDictionary?
+    
+    
+    var inviteFriendsCtrl = FollowersViewController()
+    //    var newEventId: NSString!
+    //    var apiCtrl = APIAddEventController()
+    
+    //    func didReceiveAPIResults(results: AnyObject!) {
+    //        println("AddEventViewController#didReceiveAPIResults")
+    //        newEventId = results.objectForKey("id").stringValue as String
+    //        println(newEventId)
+    ////        println(results)
+    //    }
+    
     override func viewDidLoad() {
-        startTextField.hidden = true
+        startTexTField.hidden = true
         
         endTextField.hidden = true
         
-        startTextField.userInteractionEnabled = false
+        startTexTField.userInteractionEnabled = false
         
         endTextField.userInteractionEnabled = false
         
@@ -44,8 +130,8 @@ class CreateEventViewController: UIViewController, UITextFieldDelegate
         
         // Do any additional setup after loading the view.
         
-        endDatePicker.hidden = true
-        startDatePicker.hidden = true
+        hideEndDatePicker()
+        hideStartDatePicker()
         
     }
     
@@ -55,128 +141,47 @@ class CreateEventViewController: UIViewController, UITextFieldDelegate
     }
     
     
-    @IBOutlet var nameTextField: UITextField!
-    
-    @IBOutlet var descTextField: UITextView!
-    
-    
-    @IBOutlet var venueTextField: UITextField!
-    
-    @IBOutlet var locationTextField: UITextField!
-    
-    @IBOutlet var startTextField: UITextField!
-    
-    @IBOutlet var endTextField: UITextField!
-    
-    @IBOutlet var startDatePicker: UIDatePicker!
-    
-    @IBOutlet var endDatePicker: UIDatePicker!
-    
-    @IBAction func startDateButton(sender: AnyObject) {
-        hideEndDatePicker()
-        showStartDatePicker()
-    }
-    
-    @IBAction func endDateButton(sender: AnyObject) {
-        hideStartDatePicker()
-        showEndDatePicker()
-    }
-    
-    @IBAction func confirmDates(sender: AnyObject) {
-        startTextField.hidden = false
-        
-        endTextField.hidden = false
-        
-        startTextField.text = "\(startDatePicker.date)"
-        endTextField.text = "\(endDatePicker.date)"
-        
-        startDatePicker.hidden = true
-        endDatePicker.hidden = true
-
-    }
- 
-    
-    func showStartDatePicker() {
-            println("showing Start Date Picker")
-            self.view.addSubview(startDatePicker)
-            startDatePicker.frame = CGRectMake(0,100,320, 50);
-            UIView.animateWithDuration(1.0, animations: animStartDatePicker)
-            startDatePicker.backgroundColor = UIColor( white:1, alpha:1)
-            self.view.backgroundColor = UIColor(white:1, alpha: 1)
-            hideEndDatePicker()
-    };
-    
-    func showEndDatePicker() {
-        println("showing End Date Picker")
-        self.view.addSubview(endDatePicker)
-        endDatePicker.frame = CGRectMake(0,100,320, 50);
-        UIView.animateWithDuration(1.0, animations: animEndDatePicker)
-        endDatePicker.backgroundColor = UIColor( white:1, alpha:1)
-        self.view.backgroundColor = UIColor(white:1, alpha: 1)
-        hideStartDatePicker()
-    };
-    
-
-
-
-    var params: NSDictionary?
-    
-    
-    var inviteFriendsCtrl = FollowersViewController()
-//    var newEventId: NSString!
-//    var apiCtrl = APIAddEventController()
-
-//    func didReceiveAPIResults(results: AnyObject!) {
-//        println("AddEventViewController#didReceiveAPIResults")
-//        newEventId = results.objectForKey("id").stringValue as String
-//        println(newEventId)
-////        println(results)
-//    }
-    
-  
-    
-    
     @IBAction func addEventButton(sender: AnyObject) {
         println("print this SHIT!")
-        params = ["name": nameTextField.text, "description": descTextField.text, "start_time": startTextField.text, "end_time": endTextField.text, "venue": venueTextField.text, "location": locationTextField.text] as Dictionary
-//        apiCtrl.delegate = self
-//        apiCtrl.sendCreateEventInfo(params)
+        params = ["name": nameTextField.text, "description": descTextField.text, "start_time": startTexTField.text, "end_time": endTextField.text, "venue": venueTextField.text, "location": locationTextField.text] as Dictionary
+        //        apiCtrl.delegate = self
+        //        apiCtrl.sendCreateEventInfo(params)
         println(params)
         
-//        var userDefaults:NSUserDefaults = NSUserDefaults.standardUserDefaults()
-//
-//        var eventList:NSMutableArray! = userDefaults.objectForKey("eventList") as? NSMutableArray
-//
-//        var dataSet:NSMutableDictionary = NSMutableDictionary()
-//        dataSet.setObject(nameTextField.text, forKey: "name")
-//        dataSet.setObject(descTextField.text, forKey: "description")
-//        dataSet.setObject(startTextField.text, forKey: "start_time")
-//        dataSet.setObject(endTextField.text, forKey: "end_time")
-//        dataSet.setObject(venueTextField.text, forKey: "venue")
-//        dataSet.setObject(locationTextField.text, forKey: "location")
-//
-//        if ((eventList) != nil){
-//            var newMutableList:NSMutableArray = NSMutableArray();
-//
-//            for dict:AnyObject in eventList!{
-//                newMutableList.addObject(dict as NSDictionary)
-//            }
-//
-//            userDefaults.removeObjectForKey("eventList")
-//            newMutableList.addObject(dataSet)
-//            userDefaults.setObject(newMutableList, forKey: "eventList")
-//
-//
-//        }else{
-//            userDefaults.removeObjectForKey("eventList")
-//            eventList = NSMutableArray()
-//            eventList!.addObject(dataSet)
-//            userDefaults.setObject(eventList, forKey: "eventList" )
-//        }
-//
-//        userDefaults.synchronize()
-//
-//        self.navigationController.popToRootViewControllerAnimated(true)
+        //        var userDefaults:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        //
+        //        var eventList:NSMutableArray! = userDefaults.objectForKey("eventList") as? NSMutableArray
+        //
+        //        var dataSet:NSMutableDictionary = NSMutableDictionary()
+        //        dataSet.setObject(nameTextField.text, forKey: "name")
+        //        dataSet.setObject(descTextField.text, forKey: "description")
+        //        dataSet.setObject(startTextField.text, forKey: "start_time")
+        //        dataSet.setObject(endTextField.text, forKey: "end_time")
+        //        dataSet.setObject(venueTextField.text, forKey: "venue")
+        //        dataSet.setObject(locationTextField.text, forKey: "location")
+        //
+        //        if ((eventList) != nil){
+        //            var newMutableList:NSMutableArray = NSMutableArray();
+        //
+        //            for dict:AnyObject in eventList!{
+        //                newMutableList.addObject(dict as NSDictionary)
+        //            }
+        //
+        //            userDefaults.removeObjectForKey("eventList")
+        //            newMutableList.addObject(dataSet)
+        //            userDefaults.setObject(newMutableList, forKey: "eventList")
+        //
+        //
+        //        }else{
+        //            userDefaults.removeObjectForKey("eventList")
+        //            eventList = NSMutableArray()
+        //            eventList!.addObject(dataSet)
+        //            userDefaults.setObject(eventList, forKey: "eventList" )
+        //        }
+        //
+        //        userDefaults.synchronize()
+        //
+        //        self.navigationController.popToRootViewControllerAnimated(true)
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
@@ -187,55 +192,55 @@ class CreateEventViewController: UIViewController, UITextFieldDelegate
     }
     
     
-//
+    //
     
     
-//    func textFieldShouldBeginEditing(textField: UITextField!) -> Bool {
-//        if (textField ==  startTextField) {
-//            showDatePicker()
-//            var start_time_from_date_picker = startTextField
-//            println("selected start time field")
-//            println(startDatePicker.date)
-//            return false;
-//        }
-//        else if( textField ==  endTextField){
-//            showDatePicker()
-//            var end_time_from_date_picker = endTextField
-//            println("selected end time field")
-//            println(endDatePicker.date)
-//            return false;
-//        }
-//        else {
-//        hideDatePicker()
-//
-//        return true;
-//        }
-//    }
+        func textFieldShouldBeginEditing(textField: UITextField!) -> Bool {
+            if (textField ==  startTexTField) {
+                showDatePicker()
+                hideEndDatePicker()
+                var start_time_from_date_picker = startTexTField
+                println("selected start time field")
+                println(datePicker.date)
+                return false;
+            }
+            else if( textField ==  endTextField){
+                showEndDatePicker()
+                hideStartDatePicker()
+                var end_time_from_date_picker = endTextField
+                println("selected end time field")
+                println(endDatePicker.date)
+                return false;
+            }
+            else {
     
-//    func startDatePickerDidEndEditing(textField: UITextField!) -> Bool {
-//        if (textField ==  startTextField) {
-//            startTextField.text = "\(startTimeDatePicker.date)"
-//            println("selected start time field")
-//            println(startTimeDatePicker.date)
-//            hideDatePicker();
-//            return false;
-//        }
-//        else if( textField ==  endTextField){
-//            showDatePicker();
-//            var end_time_from_date_picker = endTextField
-//            println("selected end time field")
-//            return false;
-//        }
-//        else {
-//            hideDatePicker()
-//
-//            return true;
-//        }
-//    }
+            return true;
+            }
+        }
     
+    //    func datePickerDidEndEditing(textField: UITextField!) -> Bool {
+    //        if (textField ==  startTextField) {
+    //            startTextField.text = "\(startTimeDatePicker.date)"
+    //            println("selected start time field")
+    //            println(startTimeDatePicker.date)
+    //            hideDatePicker();
+    //            return false;
+    //        }
+    //        else if( textField ==  endTextField){
+    //            showDatePicker();
+    //            var end_time_from_date_picker = endTextField
+    //            println("selected end time field")
+    //            return false;
+    //        }
+    //        else {
+    //            hideDatePicker()
+    //
+    //            return true;
+    //        }
+    //    }
     
     func animStartDatePicker() {
-        startDatePicker.backgroundColor = UIColor( white:1, alpha:1)
+        datePicker.backgroundColor = UIColor( white:1, alpha:1)
         self.view.backgroundColor = UIColor(white:1, alpha: 1)
     }
     
@@ -246,7 +251,7 @@ class CreateEventViewController: UIViewController, UITextFieldDelegate
     
     
     func hideStartDatePicker() {
-        startDatePicker.frame = CGRectMake(0,-250,320,260)
+        datePicker.frame = CGRectMake(0,-250,320,260)
         
     }
     
@@ -254,7 +259,12 @@ class CreateEventViewController: UIViewController, UITextFieldDelegate
         endDatePicker.frame = CGRectMake(0,-250,320,260)
     }
     
-}
     
+    
+    
+    
+    
+}
+
 
 
