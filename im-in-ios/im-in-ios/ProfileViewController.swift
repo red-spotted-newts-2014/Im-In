c774c55d5011c05041a3dd942625045c76d41911
elz <joelyawili@hotmail.com>
2014-08-20 23:15:39 -0400

    edited xcode profile view controller and api profile view controller.. not working yet until rails is done

--- im-in-ios/im-in-ios/ProfileViewController.swift
+++ im-in-ios/im-in-ios/ProfileViewController.swift
@@ -8,6 +8,51 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, APIProfileControllerProtocol {
 
+    @IBOutlet var firstNameField: UITextField!
+    @IBOutlet var lastNameField: UITextField!
+    @IBOutlet var usernameField: UITextField!
+    @IBOutlet var phoneField: UITextField!
+    @IBOutlet var emailField: UITextField!
+    
+    var profileData: NSDictionary
+    
+    var apiCtrl = APIProfileViewController()
+    
+    func didReceiveAPIResults(results: NSDictionary) {
+        println("FollowingViewController#didReceiveAPIResults")
+        profileData = results
+        println(profileData)
+//        dispatch_async(dispatch_get_main_queue(),{
+//            self.tableView.reloadData()
+//        })
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        firstNameField.userInteractionEnabled = false
+        lastNameField.userInteractionEnabled = false
+        usernameField.userInteractionEnabled = false
+        phoneField.userInteractionEnabled = false
+        emailField.userInteractionEnabled = false
+
+        
+        firstNameField.text = profileData.objectForKey("name") as String
+        lastNameField.text = profileData.objectForKey("username") as String
+        usernameField.text = profileData.objectForKey("description") as String
+        phoneField.text = profileData.objectForKey("venue") as String
+        emailField.text = profileData.objectForKey("location") as String
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
 }
+
+
+
